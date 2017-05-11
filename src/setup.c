dce8e676f2781f20d226cbf4f360aceae09146e3
isaac.overcast@gmail.com <isaac.overcast@gmail.com>
2017-05-11 15:21:52 -0400

    removed reference to hashtab subdirectory

--- src/setup.c
+++ src/setup.c
@@ -36,7 +36,6 @@
 #include "msprior.h"
 #include "whiteSpaces.h"
 #include "initvars.h"
-#include "hashtab/hashtab.h"
 #include "stringUtils.h"	/* for cmatrix, uniqueStrings */
 
 #define LNSZ 256		/* max length of a line */
